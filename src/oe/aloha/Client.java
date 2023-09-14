2b69e8a6e28017c0b95e07c49bc6a8e68ea9b868
helloimalastair <37487746+helloimalastair@users.noreply.github.com>
2023-09-14 13:44:46 +0200

    client/server sep

---
+++ src/oe/aloha/Client.java
@@ -0,0 +1,7 @@
+package oe.aloha;
+
+public class Client {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
--- src/oe/aloha/Server.java
+++ src/oe/aloha/Server.java
@@ -1,6 +1,6 @@
 package oe.aloha;
 
-public class Main {
+public class Server {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
