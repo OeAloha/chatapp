b319e56893ddd5bcf811b7eeb8fed22001f484d8
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2023-09-15 22:02:06 +0200

    updating ClientModule and Client

--- src/oe/aloha/Client.java
+++ src/oe/aloha/Client.java
@@ -1,7 +1,21 @@
 package oe.aloha;
 
+import oe.aloha.NetværkController.ClientModule;
+import oe.aloha.NetværkController.ServerModule;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Client {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	public static void main(String[] args) throws IOException {
+		ClientModule clientModule = new ClientModule();
+		Thread clientThread = new Thread(clientModule);
+		clientThread.start();
+
+		Scanner scanner = new Scanner(System.in); // scanner burde ikke være i controller laget
+		String messageText = scanner.nextLine();
+
+		ClientModule cm = new ClientModule();
+		cm.sendMessage(messageText);
 	}
 }
--- src/oe/aloha/NetværkController/ClientModule.java
+++ src/oe/aloha/NetværkController/ClientModule.java
@@ -1,13 +1,59 @@
 package oe.aloha.NetværkController;
 
-public class ClientModule {
+import oe.aloha.Entities.Packet;
+import oe.aloha.Entities.packet.Message;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class ClientModule implements Runnable {
+    Socket socket;
+    ObjectInputStream input;
+    ObjectOutputStream output;
+    public ClientModule() throws IOException {
+         socket = new Socket("localhost",8080);
+         input = new ObjectInputStream(socket.getInputStream());
+         output = new ObjectOutputStream(socket.getOutputStream());
+    }
 
 
+    public void sendMessage(String messageText) throws IOException {
+        Message message = new Message(messageText);
+        output.writeObject(message);
+    }
 
+    public void recieveMessage(){
+        new Thread(() -> {
+            try {
+                while (true) {
+                    Packet response = (Packet) input.readObject();
+                    System.out.println("Response from someone: " + response);
 
+                    socket.close();
+                }
+            } catch (SocketException e) {
+                System.out.println("Server is shutting down");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
 
+    }
 
+    @Override
+    public void run() {
 
-
-
+    }
 }
+
+
+
+
+
+
+
+
+
