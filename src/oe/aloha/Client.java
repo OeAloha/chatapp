a20be8a77e2988ec76ce9d32e71422a41d36ad94
helloimalastair <37487746+helloimalastair@users.noreply.github.com>
2023-09-25 11:32:52 +0200

    test scaffold

--- src/oe/aloha/Client.java
+++ src/oe/aloha/Client.java
@@ -12,7 +12,7 @@ public class Client {
 	public static void start(InputStream in, PrintStream print) {
 		print.println("Welcome to the chat! You can write to other people that is connected.");
 
-		ClientModule clientModule = new ClientModule();
+		ClientModule clientModule = new ClientModule(print);
 		Thread clientThread = new Thread(clientModule);
 		clientThread.start();
 
@@ -53,41 +53,6 @@ public class Client {
 
 
 	public static void main(String[] args) {
-
-
-		System.out.println("Welcome to the chat! You can write to other people that is connected.");
-		ClientModule clientModule = new ClientModule();
-		Thread clientThread = new Thread(clientModule);
-		clientThread.start();
-
-		System.out.println("Use exit to exit the chat.");
-
-		Scanner scanner = new Scanner(System.in);
-		while (true) {
-			String messageText = scanner.nextLine().trim();
-
-			switch (messageText) {
-				case "debug": {
-					boolean debugMode = clientModule.toggleDebug();
-					System.out.println("Debug mode is now " + (debugMode ? "on" : "off") + ".");
-					break;
-				}
-				case "exit": {
-					System.out.println("Exiting Chat...");
-					scanner.close();
-					System.exit(0);
-					break;
-				}
-				default: {
-					if (!messageText.isEmpty()) {
-						try {
-							clientModule.sendMessage(messageText);
-						} catch (IOException e) {
-							throw new RuntimeException(e);
-						}
-					}
-				}
-			}
-		}
+		start(System.in, System.out);
 	}
 }
